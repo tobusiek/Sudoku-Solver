bcde95657cdb4eb07b18c0d367ab5425bd631b7b
tobusiek <s.jasiek.gaming@gmail.com>
2022-09-23 15:48:58 +0200

    solver partially done

--- solver.py
+++ solver.py
@@ -1,11 +1,60 @@
-from abc import ABC
-from .board import Board
+from abc import ABC, abstractmethod
+from board import Board
 
 
 class BoardSolver(ABC):
+    """Abstract class for board solvers."""
     def __init__(self, board: Board) -> None:
-        self.board = board
+        self._current_board = board
+        self._solving_board = board
+    
+
+    def _update_current_board(self) -> None:
+        """Updating the current board with solved one."""
+        self._current_board = self._solving_board
+
+    
+    @property
+    def board(self) -> Board:
+        """Returns current board."""
+        return self._current_board
+
+    
+    @abstractmethod
+    def _get_possible_digits(self) -> list[list[list[int]]]:
+        """Calculating and returning possible digits for each empty cell."""
+        pass
 
 
-class DefaultSolver(BoardSolver):
-    pass
+    @abstractmethod
+    def solve(self) -> None:
+        """Calls get_possile_digits method and inputs correct digits in cells
+        if board is valid, until the board is solved. After that, it updates
+        the current board with solved one."""
+        pass
+
+
+class BasicSolver(BoardSolver):
+    """Basic solver method for sudoku board."""
+    
+    def __init__(self, board: Board) -> None:
+        super().__init__(board)
+    
+
+    def _input_single_candidates(self, possible: list[list[list[int]]]) -> None:
+        """Checks if there are single candidates to input
+        and updates the solving board"""
+        for i, row in enumerate(possible):
+            for j, col in possible[row]:
+                if len(row[col]) == 1:
+                    self._solving_board._current_board[i][j] = row[col[0]]
+
+
+    
+    def _get_possible_digits(self) -> list[list[list[int]]]:
+        pass
+
+
+    def solve(self) -> None:
+        possible = self._get_possible_digits()
+        pass
